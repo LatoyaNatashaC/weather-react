2f363ab90c40513f25cd2d5d4b0a961df78482ec
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-27 10:35:22 -0700

    form created

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,10 @@
 import React from "react";
-import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}ºC`
-    );
-  }
-  let apiKey = `4f3b0tf3219b4c7758082d0o48eabbbe`;
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+export default function Weather() {
+  return (
+    <form>
+      <input type="search" placeholder="Enter a city.." />
+      <button type="Submit">Search</button>
+    </form>
+  );
 }
