0409e5c06865c8f34bcf48615bae6b0f5369dd8e
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-27 08:38:51 -0700

    ${props.city} added to apiUrl

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ export default function Weather(props) {
     );
   }
   let apiKey = `4f3b0tf3219b4c7758082d0o48eabbbe`;
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.city}k&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
   return <h2>Hello from Weather</h2>;
