aef442eb43e9b7e37867857de01dd4c4f03813a1
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-27 18:40:58 -0700

    footer with links to gitHub & Netlify added

--- src/Weather.js
+++ src/Weather.js
@@ -2,9 +2,40 @@ import React from "react";
 
 export default function Weather() {
   return (
-    <form>
-      <input type="search" placeholder="Enter a city.." />
-      <button type="Submit">Search</button>
-    </form>
+    <div className="Weather">
+      <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+        <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
+      </a>
+      <form>
+        <input type="search" placeholder="Enter a city.." />
+        <button type="Submit">Search</button>
+      </form>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/LatoyaNatashaC"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Latoya Natasha Campbell
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/LatoyaNatashaC"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://latoyas-weather-react-app.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
   );
 }
