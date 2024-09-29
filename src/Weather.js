bca389d80f8819276412e4b1d7b32f8399262581
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-29 09:28:06 -0700

    props are now passing through default function Weather()

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState("");
 
