49be44f36fd492489b90f119204d9caf02c45ec6
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-29 14:33:00 -0700

    bootstrap imported

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
