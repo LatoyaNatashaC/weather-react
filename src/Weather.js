d32c83313fbdb0ad488c3377c9ee5e5236c7610f
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-09-30 00:09:18 -0700

    current weather and forecast now displaying

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
   function displayWeather(response) {
-    console.log(response.data);
-    alert(response.data.temperature.current);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
   }
 
   function handleSubmit(event) {
@@ -26,57 +38,67 @@ export default function Weather(props) {
     axios.get(apiUrl).then(displayWeather);
   }
 
-  return (
-    <div className="Weather">
-      <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-        <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
-      </a>
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9 ">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control search-input"
-              onChange={updateCity}
-            />
-          </div>
-          <div className="col-3 p-0">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      "Search.."
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/LatoyaNatashaC"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Latoya Natasha Campbell
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/LatoyaNatashaC"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://latoyas-weather-react-app.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+          <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
         </a>
-      </footer>
-    </div>
-  );
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9 ">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control search-input"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3 p-0">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast
+          coordinates={weatherData.coordinates}
+          city={weatherData.city}
+        />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/LatoyaNatashaC"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Latoya Natasha Campbell
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LatoyaNatashaC"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://latoyas-weather-react-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
